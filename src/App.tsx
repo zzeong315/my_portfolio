0d1a39afa4cde8e696a316e319fc4cf4ade769a1
zzeong <zzeong315@gamil.com>
2023-05-04 16:11:15 +0900

    my portfolio

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='bg-amber-50 w-full h-[100vh]'>
+      <nav className='flex justify-between items-center h-20 px-12 bg-fuchsia-50'>
+        <div>Home</div>
+        <ul className='flex space-x-8'>
+          <li>Project</li>
+          <li>About</li>
+        </ul>
+      </nav>
     </div>
   );
 }
